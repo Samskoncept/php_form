03a1e67af1f1328ef5f65fdb4d0b4b2349426700
samskoncept <samuelfrancis90@hotmail.com>
2022-10-15 08:47:43 +0100

    First commit

---
+++ user_data.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+
+echo 'Name:', $name, '<br>';
+echo 'Email:', $email, '<br>';
+echo 'Date of Birth:', $dob, '<br>';
+echo 'Gender:', $gender, '<br>';
+echo 'Country', $country;
+
+
+?>
